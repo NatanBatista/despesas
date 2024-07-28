0e066b564aad15810968544ec1216f30af6903ce
Natan Batista <natanbatistaa21@gmail.com>
2024-07-28 18:49:42 -0300

    Adding modal transcaction form

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,67 @@
+import 'package:despesas/components/home/transactions_form.dart';
+import 'package:despesas/components/home/transactions_list.dart';
+import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> _transactions = [
+    ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: "",
+      title: title,
+      value: value,
+      date: DateTime.now()
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openModalTransactionForm(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionsForm(onSubmit: _addTransaction);
+        },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("OI"),),
+      appBar: AppBar(
+        title: const Text("App BAR"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Card(
+              child: Text("Oi"),
+            ),
+            TransactionsList(transactions: _transactions,),
+          ]
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _openModalTransactionForm(context)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
